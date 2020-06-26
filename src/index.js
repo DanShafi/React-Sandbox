fc5f1ad591e0daa91e9a1b0b4d7760984351fefe
Danish Shafi <danish.shafi@live.co.uk>
2020-06-27 00:55:44 +0100

    First function for DOM render

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import Stylesheet from "../src/style.css"
 
-ReactDOM.render(<h1>Hello, World!</h1>, document.querySelector('#root'))
+function MyInfo() {
+    return (
+        <>
+        <h1>Danish Shafi</h1>
+        <p>I am an aspiring frontend developer.</p>
+        <ul>
+            <li>Dubai</li>
+            <li>America</li>
+            <li>Maldives</li>
+        </ul>
+        </>
+    )
+}
+
+ReactDOM.render(<MyInfo />, document.querySelector('#root'));
