ae9e7f60b30473c66bba47c5e0ec13777ef87fbc
Danish Shafi <danish.shafi@live.co.uk>
2020-07-03 23:30:01 +0100

    Class-based components and State included

--- src/App.js
+++ src/App.js
@@ -1,22 +1,76 @@
-import React from "react"
+import React, {Component} from "react"
 import Stylesheet from "../src/style.css"
 import MainContent from "../src/components/MainContent"
 import Footer from "../src/components/Footer"
 import Product from "./components/Product"
 import productsData from "../src/components/ProductsData"
+// function App() {
 
-function App() {
+//     const productComponents = productsData.map(product => <Product key={product.id} 
+//         product={product} />)
 
-    const productComponents = productsData.map(product => <Product key={product.id} 
-    product={product} />)
+//     return (
+//     <div className="container">
+//         <MainContent />
+//         {productComponents}
+//         <Footer />
+//     </div>
+//     )
+// }
 
-    return (
-    <div className="container">
-        <MainContent />
-        {productComponents}
-        <Footer />
-    </div>
-    )
+// class App extends React.Component {
+//     render() {
+//         const productComponents = productsData.map(product => <Product key={product.id} 
+//             product={product} />)
+//         return (
+//         <div className="container">
+//             <MainContent />
+//             {productComponents}
+//             <Footer />
+//         </div>
+//         )
+//     }
+// }
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header username="Danish"/>
+                <Greeting />
+            </div>
+        )
+    }
+}
+
+class Header extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            username: "DanShafi",
+        }
+    }
+
+    render() {
+        return (
+            <header>
+                <p>Welcome, {this.state.username}!</p>
+            </header>
+        )
+    }
+}
+
+class Greeting extends Component {
+    render() {
+        const date = new Date()
+        const hours = date.getHours()
+        let timeOfDay
+            return (
+                <div>
+                    <h1>Hello, it's {hours}:00PM.</h1>
+                </div>
+        )
+    }
 }
 
 export default App
